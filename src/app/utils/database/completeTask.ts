484fc0a3b7b41b8ad836ce69656d54e342db25e5
gbtsui <gabriel.tsui@protonmail.com>
2024-12-18 16:56:29 -0500

    feat: complete task script

---
+++ src/app/utils/database/completeTask.ts
@@ -0,0 +1,44 @@
+import {prisma} from "@/app/utils/database/db";
+
+export default async function completeTask(task_id: number) {
+    try {
+        const task = await prisma.task.findUnique(
+            {
+                where: {
+                    id: task_id,
+                }
+            }
+        )
+        if (!complete) {
+            console.log("this shouldn't even be humanly possible bruh :sob:")
+            throw new Error("NONEXISTENT_TASK")
+            //it shouldn't even be possible to reach here but i need to do error handling anyways
+        }
+
+        const complete = task?.completed
+
+
+
+        //kinda gross ngl but it is what it is
+
+        const result = await prisma.task.update({
+            where: {
+                id: task_id
+            },
+            data: {
+                completed: !complete
+            }
+        })
+
+        if (result) {
+            return result
+        } else {
+            return null
+        }
+    } catch (error) {
+        console.error(error)
+        if (error instanceof Error) {
+            return error
+        }
+    }
+}
