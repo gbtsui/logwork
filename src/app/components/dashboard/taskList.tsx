9da96b16e21872b61705395076416646663f346a
gbtsui <gabriel.tsui@protonmail.com>
2025-01-16 11:16:49 -0500

    fix: loading page

--- src/app/components/dashboard/taskList.tsx
+++ src/app/components/dashboard/taskList.tsx
@@ -3,18 +3,30 @@
 import {Task} from "@prisma/client";
 import TaskCompleteButton from "@/app/components/dashboard/taskCompleteButton";
 import {useTaskStore} from "@/app/utils/store/taskStore"
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function TaskList() {
     const tasks: Task[] = useTaskStore((state) => state.tasks);
     const fetchTaskList = useTaskStore((state) => state.fetchTaskList);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetchTaskList();
+        const fetchTaskListEffect = async () => {
+            setLoading(true);
+            await fetchTaskList();
+        }
+        fetchTaskListEffect();
+        setLoading(false);
     })
 
     return (
         <div className="flex flex-row">
+            {
+                loading &&
+                (<div>
+                    <h1 className={"text-3xl text-center"}>loading...</h1>
+                </div>)
+            }
             {
                 tasks &&
                 tasks.map(task =>
@@ -26,11 +38,12 @@ export default function TaskList() {
                     </div>)
             }
             {
-                tasks.length === 0 &&
+                tasks.length === 0 && !loading &&
                 (<div>
                     <h1 className={"text-3xl text-center"}>no tasks found; go and make some</h1>
                 </div>)
             }
+
         </div>
     )
 }
