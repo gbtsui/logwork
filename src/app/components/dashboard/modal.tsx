876dbf4eea0cfe69343bf1342efe8a70944ad568
gbtsui <gabriel.tsui@protonmail.com>
2025-01-18 13:32:26 -0500

    feat: task deletion
    also adjusted modal aesthetics

--- src/app/components/dashboard/modal.tsx
+++ src/app/components/dashboard/modal.tsx
@@ -8,7 +8,7 @@ export default function Modal({ isOpen, onClose, title, children}: {isOpen: bool
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 overflow-hidden h-screen w-full flex items-center justify-center z-50">
-            <div className="p-6 rounded-lg shadow-lg w-96 overflow-auto">
+            <div className="p-6 bg-background rounded-lg shadow-lg w-96 overflow-auto">
                 <div className="flex justify-between items-center mb-4">
                     <h3 className="text-xl font-semibold">{title}</h3>
                     <button onClick={onClose} className="text-gray-400 hover:text-gray-500">
@@ -19,7 +19,7 @@ export default function Modal({ isOpen, onClose, title, children}: {isOpen: bool
                     {children}
                 </div>
                 <div className="flex justify-end space-x-2 pt-4">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={onClose}>
+                    <button className="bg-foreground text-background px-4 py-2 rounded hover:bg-red-600 hover:text-foreground hover:px-5 hover:py-3 transition-all" onClick={onClose}>
                         Close
                     </button>
                 </div>
--- src/app/components/dashboard/taskEntry.tsx
+++ src/app/components/dashboard/taskEntry.tsx
@@ -2,14 +2,35 @@
 
 import {Task} from "@prisma/client";
 import TaskCompleteButton from "@/app/components/dashboard/taskCompleteButton";
+import {useState} from "react";
+import Modal from "@/app/components/dashboard/modal";
+import deleteTask from "@/app/utils/database/deleteTask";
+import {useTaskStore} from "@/app/utils/store/taskStore";
 
 export default function TaskEntry({task}: {task: Task}) {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const deleteTaskFromStore = useTaskStore((state) => state.deleteTask);
+
     return (
-        <div className={"border-2 border-foreground rounded-bl-lg rounded-tr-lg p-2 m-1 w-44 h-44"}>
-            <h1 className={"text-xl"}>{task.task_name}</h1>
-            <p>{task.task_description}</p>
-            <TaskCompleteButton task_id={task.id} completed={task.completed}/>
-            <p className={"text-xs"}>Created at {task.created_at.toString()}</p>
-        </div>
+        <>
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title={"delet?"}>
+                <p>are you sure you want to delete "{task.task_name}"?</p>
+                <button onClick={async () => {
+                    await deleteTask(task.id);
+                    deleteTaskFromStore(task);
+                    setIsOpen(false);
+                }}>
+                    DELET THIS !!!!
+                </button>
+            </Modal>
+
+            <div className={"border-2 border-foreground rounded-bl-lg rounded-tr-lg p-2 m-1 w-44 h-44"}>
+                <h1 className={"text-xl"}>{task.task_name}</h1>
+                <p>{task.task_description}</p>
+                <TaskCompleteButton task_id={task.id} completed={task.completed}/>
+                <p className={"text-xs"}>Created at {task.created_at.toString()}</p>
+                <button onClick={() => setIsOpen(true)}> delet :( </button>
+            </div>
+        </>
     )
 }
