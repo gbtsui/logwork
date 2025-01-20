f19d90eb4a25d52236269d20b72fd115a8e8007a
gbtsui <gabriel.tsui@protonmail.com>
2025-01-20 10:51:02 -0500

    feat: tasklist prisma error handling

--- src/app/utils/database/getTaskList.ts
+++ src/app/utils/database/getTaskList.ts
@@ -2,15 +2,21 @@
 import {prisma} from "@/app/utils/database/db";
 
 export default async function getTaskList(user_id: number) {
-    const tasks = await prisma.task.findMany({
-        where: {
-            owner_id: user_id,
+    try {
+        const tasks = await prisma.task.findMany({
+            where: {
+                owner_id: user_id,
+            }
+        })
+
+        if (tasks.length === 0) {
+            return []
         }
-    })
-
-    if (tasks.length === 0) {
-        return []
+        return tasks
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message)
+            return error
+        }
     }
-
-    return tasks
 }
