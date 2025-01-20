3e2dc77794fb79dc3cf0d7166281987bbb9b7b73
gbtsui <gabriel.tsui@protonmail.com>
2025-01-19 19:28:17 -0500

    feat: task counter

--- src/app/components/dashboard/dashboardContainer.tsx
+++ src/app/components/dashboard/dashboardContainer.tsx
@@ -1,8 +1,5 @@
 "use client"
 import TaskList from "@/app/components/dashboard/taskList";
-import CreateTaskButton from "@/app/components/dashboard/createTaskButton";
-//import Modal from "@/app/components/dashboard/modal";
-
 
 export default function DashboardContainer() {
 
@@ -11,10 +8,7 @@ export default function DashboardContainer() {
             <h1 className={"text-7xl text-center p-3 m-3"}>
                 dashboard
             </h1>
-            <h2 className={"ml-8"}>
-                <CreateTaskButton/>
-                <TaskList/>
-            </h2>
+            <TaskList/>
         </div>
     )
 }
--- src/app/components/dashboard/taskList.tsx
+++ src/app/components/dashboard/taskList.tsx
@@ -5,6 +5,7 @@ import {useTaskStore} from "@/app/utils/store/taskStore"
 import {useEffect, useState} from "react";
 import LoadingSkeleton from "@/app/components/universal/loadingSkeleton";
 import TaskEntry from "@/app/components/dashboard/taskEntry";
+import CreateTaskButton from "@/app/components/dashboard/createTaskButton";
 //import Modal from "@/app/components/dashboard/modal";
 
 
@@ -14,7 +15,6 @@ export default function TaskList() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-
         const fetchTaskListEffect = async () => {
             try {
                 setLoading(true);
@@ -28,25 +28,37 @@ export default function TaskList() {
         fetchTaskListEffect();
     }, [fetchTaskList]);
 
-
+    const overdueTasks = tasks.filter(task => {return (task.due_at.getTime() - Date.now()) < 0 && !task.completed}).length
+    const dueSoonTasks = tasks.filter(task => {return ((task.due_at.getTime() - Date.now()) < 3600000 && (task.due_at.getTime() - Date.now()) > 0 && !task.completed)}).length
+    const completedTasks = tasks.filter(task => {return task.completed}).length
 
     if (loading) {
         return <LoadingSkeleton />;
     }
 
     return (
-        <div className="flex flex-row flex-wrap justify-center">
-            {
-                tasks &&
-                tasks.map(task => <TaskEntry task={task} key={task.id}/>)
-            }
-            {
-                tasks.length === 0 && !loading &&
-                (<div>
-                    <h1 className={"text-3xl text-center"}>no tasks found; go and make some</h1>
-                </div>)
-            }
+        <>
+            <div className={"flex justify-between p-2 pr-7 bg-forestgreen mx-7 my-3 rounded-tr-3xl rounded-bl-3xl"}>
+                <div className={"flex flex-col bg-cream p-1.5 rounded-tr-xl rounded-bl-xl"}>
+                    <h2 className={"text-xl font-bold align-middle inline-block text-red-500"}>{overdueTasks} overdue</h2>
+                    <h2 className={"text-xl font-bold align-middle inline-block text-amber-500"}>{dueSoonTasks} due soon</h2>
+                    <h2 className={"text-xl font-bold align-middle inline-block text-forestgreen"}>{completedTasks} complete</h2>
+                </div>
+                <CreateTaskButton/>
+            </div>
+            <div className="flex flex-row flex-wrap justify-center w-full mb-32">
+                {
+                    tasks &&
+                    tasks.map(task => <TaskEntry task={task} key={task.id}/>)
+                }
+                {
+                    tasks.length === 0 && !loading &&
+                    (<div>
+                        <h1 className={"text-3xl text-center"}>no tasks found; go and make some</h1>
+                    </div>)
+                }
 
-        </div>
+            </div>
+        </>
     )
 }
